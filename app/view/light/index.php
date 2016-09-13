828cce44ae8e842c1f942045cd6f3cc38089a355
tapasa30 <pablobague@gmail.com>
2016-09-13 12:09:05 +0200

    MVC

--- app/view/light/index.php
+++ app/view/light/index.php
@@ -3,9 +3,39 @@ $pageTitle = "Inicio";
 include_once("app/view/light/includes/header.php");
 ?>
 
-			<div class = "roller"></div>
-			<div class = "categories"></div>
+			<div class = "roller">
+				<img style = "width: 100%; height: 200px; " src="https://thesingleplayermode.files.wordpress.com/2013/05/pokemon-banner.jpg">
+			</div>
+			<div class = "categories">
+				<nav class = "menu">
+			<ul>
+				<li><a href="index.php">Humor</a></li>
+				<li><a href="index.php">Humor</a></li>
+				<li><a href="index.php">Humor</a></li>
+				<li><a href="index.php">Humor</a></li>
+			</ul>
+		</nav>
+			</div>
 			<div class = "news">
+
+				<div class = "item">
+					<div class = "itemImage">
+						<img src="https://upload.wikimedia.org/wikipedia/en/3/39/Pokeball.PNG">
+					</div>
+					<div class = "itemDescription">
+						Descripcion
+					</div>
+				</div>
+
+				<div class = "item">
+					<div class = "itemImage">
+						<img src="https://upload.wikimedia.org/wikipedia/en/3/39/Pokeball.PNG">
+					</div>
+					<div class = "itemDescription">
+						Descripcion
+					</div>
+				</div>
+
 				<div class = "item">
 					<div class = "itemImage">
 						<img src="https://upload.wikimedia.org/wikipedia/en/3/39/Pokeball.PNG">
